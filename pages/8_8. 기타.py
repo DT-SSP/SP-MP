8b1f6defefd611c444f0dd7b51c7ed15b5c5b541
yjyj2021 <yjyj041213@gmail.com>
2026-05-20 15:42:19 +0900

    8_8.기타&modules.py지역명추가수정3(5/20)

--- pages/8_8. 기타.py
+++ pages/8_8. 기타.py
@@ -163,7 +163,7 @@ with t1:
         sel_m = int(st.session_state["month"])
 
         disp_raw, meta = modules.build_table_60(df_src, sel_y, sel_m)
-        st.write(disp_raw[["구분1", "구분2"]].head(10))
+
 
         hdr1 = meta["hdr1"]
 

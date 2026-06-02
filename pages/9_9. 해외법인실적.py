e4bce57f43d5c1dc13f9ba1a86fc7743f18cc179
yjyj2021 <yjyj041213@gmail.com>
2026-06-02 15:49:39 +0900

    메모파일수정(6/02)

--- pages/9_9. 해외법인실적.py
+++ pages/9_9. 해외법인실적.py
@@ -1181,10 +1181,10 @@ with t3:
         if col_curr in c_idx:
             hdr[c_idx[col_curr]] = f"'{yy_curr}년 {used_m}월"
         if col_diff in c_idx:
-            hdr[c_idx[col_diff]] = "전월비"
+            hdr[c_idx[col_diff]] = "전월대비 증감"
         else:
             # col_diff 매핑에 실패하더라도 리스트의 맨 마지막(증감 컬럼)에 강제로 헤더 제목을 주입합니다.
-            hdr[-1] = "전월비"
+            hdr[-1] = "전월대비 증감"
 
         hdr_df   = pd.DataFrame([hdr], columns=cols)
         disp_vis = pd.concat([hdr_df, disp], ignore_index=True)
@@ -1356,10 +1356,10 @@ with t3:
         if col_curr in c_idx:
             hdr[c_idx[col_curr]] = f"'{yy_curr}년 {used_m}월"
         if col_diff in c_idx:
-            hdr[c_idx[col_diff]] = "전월비"
+            hdr[c_idx[col_diff]] = "전월대비 증감"
         else:
             # col_diff 매핑에 실패하더라도 리스트의 맨 마지막(증감 컬럼)에 강제로 헤더 제목을 주입합니다.
-            hdr[-1] = "전월비"
+            hdr[-1] = "전월대비 증감"
 
         hdr_df   = pd.DataFrame([hdr], columns=cols)
         disp_vis = pd.concat([hdr_df, disp], ignore_index=True)

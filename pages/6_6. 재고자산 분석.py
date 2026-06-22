0fd2f0ea70fa72dbb4bdd17ab2c2ff66840e5315
yjyj2021 <yjyj041213@gmail.com>
2026-06-22 11:52:41 +0900

    6.재고자산분석6(6/22)

--- pages/6_6. 재고자산 분석.py
+++ pages/6_6. 재고자산 분석.py
@@ -727,8 +727,7 @@ with t4:
 
             scatter_trace_cls = None
 
-            # 💡 [수정] Y축 범위 설정 (톤 단위, KG에서 변환됨)
-            scatter_trace_cls = {'name': '장기재고', 'color': '#ffc107', 'range': [0, 1500]}
+
 
             display_inventory_chart(df_chart_cls, bar_traces_cls, scatter_trace_cls, key="grade_inventory_chart")
 
